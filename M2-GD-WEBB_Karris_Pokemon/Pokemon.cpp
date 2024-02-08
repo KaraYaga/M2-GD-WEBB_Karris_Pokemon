cf68d5c233b066eb1b2d19eca3f26f444dbf3869
Karris WEBB <153066380+KaraYaga@users.noreply.github.com>
2024-02-08 17:47:01 +0100

    [Finished Abilities List]

--- M2-GD-WEBB_Karris_Pokemon/Pokemon.cpp
+++ M2-GD-WEBB_Karris_Pokemon/Pokemon.cpp
@@ -33,30 +33,30 @@ int Pokemon::getLife() { return mLife; }
 //CHOOSE ABILITIES FOR POKEMON
 void Pokemon::chooseAbilities() {
     vector<Ability> allAbilities = {
-        Ability("Smash", "Bludgeon your enemy with brute force!", 5),
-        Ability("Slash", "Lash out at your enemy with sharpened skill!", 5),
-        Ability("Rip", "Rip apart the flesh of your enemies!", 10),
-        Ability("Tear", "Tear apart your enemies, limb from limb!", 10),
-        Ability("", "", 0),
-        Ability("", "", 0),
-        Ability("", "", 0),
-        Ability("", "", 0),
-        Ability("", "", 0),
-        Ability("", "", 0),
-        Ability("", "", 0),
-        Ability("", "", 0),
-        Ability("", "", 0),
-        Ability("", "", 0),
-        Ability("", "", 0),
-        Ability("", "", 0),
+        Ability("Smash", "Bludgeon your enemy with brute force!", 5, 3),
+        Ability("Slash", "Lash out at your enemy with sharpened skill!", 5, 3),
+        Ability("Spit", "Spit on your enemy, blinding them and burning their skin!", 5, 3),
+        Ability("Poisionus Smoke", "Cough out black smoke that chokes your enemy!", 5, 3),
+        Ability("Noxious Gas", "Gas leeks from your Pokemon's body rendering the enemy nauseous and sick!", 5, 3),
+        Ability("Stampede", "Split your Pokemon into three and stampede towards the enemy!", 5, 3),
+        Ability("Hanged Man", "Your Pokemon begins to do a strange dance that hurts to look at!", 5, 3),
+        Ability("Armored Bullet", "Your Pokemon's flesh becomes as hard as steel as they barrel towards the enemy!", 5, 3),
+        Ability("Rip", "Rip apart the flesh of your enemies!", 10, 2),
+        Ability("Tear", "Tear apart your enemies, limb from limb!", 10, 2),
+        Ability("Crushing Hit", "Break bones and burst organs!", 10, 2),
+        Ability("Fanged Bite", "Bite into the enemy and exsanguinate them!", 10, 2),
+        Ability("Death from Above", "Fly above your enemy and torpedo towards them, hitting their body with a massive shock wave!", 10, 2),
+        Ability("Death Shriek", "Violently scream, rendering the breains of your enemies mush!", 15, 1),
+        Ability("Bloody Transmutation", "Transform the very body of your enemy, breaking bones, and splitting tendons!", 15, 1),
+        Ability("???", "It... does something???", 20, 1),
     };
     
 
-    std::cout << "Please choose your Pokemon's four Abilities! Here are all available ones :\n";
+    std::cout << "Please choose your Pokemon's four Abilities! Keep in mind, the Lower in the list, the Higher the power and the fewer uses it has before it must rest...maybe!\nHere are all available ones :\n";
 
     for (int i = 0; i < allAbilities.size(); i++)
     {
-        std::cout << "#" << (i + 1) << " : " << allAbilities[i].GetName()<<"  "<<allAbilities[i].GetDescription() << endl;
+        std::cout << "#" << (i + 1) << " : " << allAbilities[i].GetName()<<" : "<<allAbilities[i].GetDescription() << endl;
     }
 
     for (int i = 0; i < 4; i++) 
